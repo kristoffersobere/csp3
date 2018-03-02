5c039c66da7430d590c703bbf429bf63029b8129
kristoffersobere <kristoffer.sobere@gmail.com>
2018-03-03 00:00:26 +0800

    initial commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-            'profile_pic' => 'abc',
+            'profile_pic' => $data['abc'],
         ]);
     }
 }
